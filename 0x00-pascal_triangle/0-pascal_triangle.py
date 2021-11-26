c415573a7137f93545e8dba8ec0e0899409cfbc2
livejo <yoseph.kefale@gmail.com>
2021-11-26 09:40:46 +0300

    Add files via upload

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n: int):
+    ''' returns a list of pascal triangle '''
+    pascal = []
+    if n <= 0:
+        return []
+    for line in range(n):  # number of raws
+        arr = [0 for i in range(line + 1)]  # new array for each
+        for i in range(n):  # to add elements to the individual array
+            if i > line:
+                # break if it reaches the limit
+                break
+            if i == 0 or i == line:
+                # add one if it is at the beggining or at the ending
+                arr[i] = 1
+            else:
+                # add compute from perivous and add to the current list
+                arr[i] = (pascal[line - 1][i - 1] +
+                          pascal[line - 1][i])
+        pascal.append(arr)
+    return pascal
